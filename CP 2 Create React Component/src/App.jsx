a17308a8532c41f0d9fb4e3c045c878f9a4c8071
JatinbVyas <vyassjatin@gmail.com>
2025-02-24 20:53:17 +0530

    learn how to create component and import and export of that component.

--- CP 2 Create React Component/src/App.jsx
+++ CP 2 Create React Component/src/App.jsx
@@ -1,10 +1,15 @@
+import ButtonCP from "./ButtonCP"
+
 /**
  * Below is functional component.
  * When it called, it will returns H1 tag with content. 
  * Earlier class component were used but now they are not used anymore.
  */
 function App(){
-  return <h1>Hello This is React App file changes.</h1>
+  return <div>
+    <h1>Hello This is React App file changes.</h1>
+    <ButtonCP></ButtonCP>
+    </div>
 }
 
 //Below export default used to export this App function/component to use at diffrent place.
