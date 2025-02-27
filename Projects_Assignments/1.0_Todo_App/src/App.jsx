25743217fbf4f5d72c91d3fbee94deb17b628445
JatinbVyas <vyassjatin@gmail.com>
2025-02-28 00:05:45 +0530

    Todo app created this is version 1.0 just hard code data

--- Projects_Assignments/1.0_Todo_App/src/App.jsx
+++ Projects_Assignments/1.0_Todo_App/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItems1 from "./components/TodoItems1";
+import TodoItems2 from "./components/TodoItems2";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <center className="todo-container">
+      <AppName></AppName>
+      <AddTodo></AddTodo>
+      <div className="items-container">
+        <TodoItems1></TodoItems1>
+        <TodoItems2></TodoItems2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </center>
+  );
 }
 
-export default App
+export default App;
