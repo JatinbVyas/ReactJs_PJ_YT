04da994e27cde5341f6e115890414ff8140714f7
JatinbVyas <vyassjatin@gmail.com>
2025-02-28 18:52:05 +0530

    learn the use of <react.Fragment> tag

--- 4_Topic 18 Fragments/Learn_Fragments/src/App.jsx
+++ 4_Topic 18 Fragments/Learn_Fragments/src/App.jsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div>Fragments</div>;
+  return (
+    /**
+     * <React.Fragment></React.Fragment> tag is used to define that inside that tag we can define our HTML code and need to extra parrent tag element like <div> or <table> or <center>
+     */
+    <React.Fragment>
+      <h1>Healthy Foods</h1>
+      <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+      </ul>
+    </React.Fragment>
+  );
 }
 
 export default App;
