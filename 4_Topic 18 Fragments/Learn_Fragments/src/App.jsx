1883dbe43c2349d3b184f7cf1d3f779784baea14
JatinbVyas <vyassjatin@gmail.com>
2025-02-28 20:25:25 +0530

    seen the use of map method of array

--- 4_Topic 18 Fragments/Learn_Fragments/src/App.jsx
+++ 4_Topic 18 Fragments/Learn_Fragments/src/App.jsx
@@ -1,17 +1,19 @@
 function App() {
+  let foodItems = ["Dal", "Roti", "Green Vegetables", "Milk", "Salad"];
   return (
     /**
      * <React.Fragment></React.Fragment> tag is used to define that inside that tag we can define our HTML code and need to extra parrent tag element like <div> or <table> or <center>
      * Another way of React Fragment tag is used them as empty <> </>
+     * using below way we can use array element with map method to show values.
      */
     <>
       <h1>Healthy Foods</h1>
-      <ul class="list-group">
-        <li class="list-group-item">Dal</li>
-        <li class="list-group-item">Roti</li>
-        <li class="list-group-item">Green Vegetables</li>
-        <li class="list-group-item">Milk</li>
-        <li class="list-group-item">Salad</li>
+      <ul className="list-group">
+        {foodItems.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
