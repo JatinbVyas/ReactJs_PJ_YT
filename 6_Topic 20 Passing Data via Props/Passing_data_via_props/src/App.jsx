21d5032ced0acb7e2a056d381097ad1dbe54932d
JatinbVyas <vyassjatin@gmail.com>
2025-03-01 00:01:50 +0530

    seen the use of props , how it transfer from parent to child and child to grand child

--- 6_Topic 20 Passing Data via Props/Passing_data_via_props/src/App.jsx
+++ 6_Topic 20 Passing Data via Props/Passing_data_via_props/src/App.jsx
@@ -1,31 +1,21 @@
+import ErrorMessage from "./components/ErrorMessage";
+import FoodItems from "./components/FoodItems";
+
 function App() {
-  let foodItems = ["Dal", "Roti", "Green Vegetables", "Milk", "Salad"];
-  //let foodItems = [];
-  let emptyMessage = foodItems.length === 0 ? <h1>I'm hungry.</h1> : null;
+  let foodItems = ["Dal Makhani", "Roti", "Green Vegetables", "Milk", "Salad"];
 
   /**
-   * One way to do conditional rendring is if..else. statements
-   * another way is ternory opertor. ?
+   * Here foodItems define only ones in App.jsx then it pass to both child component ErrorMessage and FoodItems.
+   * For passing varriable syntaxt is as below define any variable name
+   * just like here is listofItems and assign the value in {} braces.
+   * e.g temp={foodItems}
+   * Now this foodItems array will get received in both components.
    */
-  // if (foodItems.length === 0) {
-  //   return <h1>I'm hungry.</h1>;
-  // }
   return (
-    /**
-     * <React.Fragment></React.Fragment> tag is used to define that inside that tag we can define our HTML code and need to extra parrent tag element like <div> or <table> or <center>
-     * Another way of React Fragment tag is used them as empty <> </>
-     * using below way we can use array element with map method to show values.
-     */
     <>
       <h1>Healthy Foods</h1>
-      {foodItems.length === 0 && <h1>I'm hungry.</h1>}
-      <ul className="list-group">
-        {foodItems.map((item) => (
-          <li key={item} className="list-group-item">
-            {item}
-          </li>
-        ))}
-      </ul>
+      <ErrorMessage listofItems={foodItems}></ErrorMessage>
+      <FoodItems listofItems={foodItems}></FoodItems>
     </>
   );
 }
