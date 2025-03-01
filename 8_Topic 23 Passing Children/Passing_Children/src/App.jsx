db4abdfab67eec2e749c3d2d6b200be3847e246c
JatinbVyas <vyassjatin@gmail.com>
2025-03-01 20:19:56 +0530

    seen the use of props children

--- 8_Topic 23 Passing Children/Passing_Children/src/App.jsx
+++ 8_Topic 23 Passing Children/Passing_Children/src/App.jsx
@@ -1,14 +1,26 @@
+import Container from "./components/Container";
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
 
 function App() {
   let foodItems = ["Dal Makhani", "Roti", "Green Vegetables", "Milk", "Salad"];
-
+  /**
+   * Learn the use of props children, here we created a component with name Container.jsx and in which we define <div> with some css to look good. and now in that Container component we passing other component and tags. Those taga and component can be catch at props.children at component side.
+   */
   return (
     <>
-      <h1>Healthy Foods</h1>
-      <ErrorMessage listofItems={foodItems}></ErrorMessage>
-      <FoodItems listofItems={foodItems}></FoodItems>
+      <Container>
+        <h1 style={{ textAlign: "center" }}>Healthy Foods</h1>
+        <ErrorMessage listofItems={foodItems}></ErrorMessage>
+        <FoodItems listofItems={foodItems}></FoodItems>
+      </Container>
+
+      <Container>
+        <p>
+          Above are the list of healthy foods that are good for your health and
+          well being.
+        </p>
+      </Container>
     </>
   );
 }
