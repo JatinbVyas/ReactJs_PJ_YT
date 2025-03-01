977557f05e6e588516af77b85ca5f78dd9826ad8
JatinbVyas <vyassjatin@gmail.com>
2025-03-01 22:18:37 +0530

    seen the use of function with arrow and reference.

--- 9_Topic 24 Handling Events/src/components/FoodInput.jsx
+++ 9_Topic 24 Handling Events/src/components/FoodInput.jsx
@@ -1,11 +1,14 @@
 import styles from "./FoodInput.module.css";
 const FoodInput = () => {
+  const onChangeEventCalled = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <input
       type="text"
       placeholder="Enter your food item."
       className={styles.foodInput}
-      onChange={(event) => console.log(event.target.value)}
+      onChange={onChangeEventCalled}
     ></input>
   );
 };
