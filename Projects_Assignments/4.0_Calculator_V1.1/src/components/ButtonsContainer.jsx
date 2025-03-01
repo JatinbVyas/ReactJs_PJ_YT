c29032dac3af58acb2022788c54a2f184b1307d0
JatinbVyas <vyassjatin@gmail.com>
2025-03-02 01:12:29 +0530

    new folder created for project calculator from 1.0

---
+++ Projects_Assignments/4.0_Calculator_V1.1/src/components/ButtonsContainer.jsx
@@ -0,0 +1,31 @@
+import styles from "./ButtonsContainer.module.css";
+const ButtonsContainer = () => {
+  let buttonsNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
+  return (
+    <div id="buttons-container" className={styles.calciButton}>
+      {buttonsNames.map((button) => (
+        <button className={styles.button}>{button}</button>
+      ))}
+    </div>
+  );
+};
+
+export default ButtonsContainer;
