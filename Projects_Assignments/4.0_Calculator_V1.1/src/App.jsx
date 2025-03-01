568a80f0d0a5e186fdc94e9e4296ed0628820859
JatinbVyas <vyassjatin@gmail.com>
2025-03-02 01:45:48 +0530

    completes the calculator project

--- Projects_Assignments/4.0_Calculator_V1.1/src/App.jsx
+++ Projects_Assignments/4.0_Calculator_V1.1/src/App.jsx
@@ -5,7 +5,16 @@ import Displaytxt from "./components/Displaytxt";
 function App() {
   const [calVal, setcalVal] = useState("");
   const onClick = (buttonText) => {
-    console.log(buttonText);
+    if (buttonText === "C") {
+      setcalVal("");
+    } else if (buttonText === "=") {
+      //Here eval is JS function which auto evalute string value in mathematics concepts.
+      const result = eval(calVal);
+      setcalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setcalVal(newDisplayValue);
+    }
   };
   return (
     <div id="calculator" className={styles.calci}>
