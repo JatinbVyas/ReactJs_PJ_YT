5051dbc27a730f92728a6fff6e3a3dc8fc4109ce
JatinbVyas <vyassjatin@gmail.com>
2025-03-01 23:52:46 +0530

    learn the basic concept of useState hook

--- 11_Topic 26 Managing State/src/App.jsx
+++ 11_Topic 26 Managing State/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "./components/Container";
 import ErrorMessage from "./components/ErrorMessage";
 import FoodInput from "./components/FoodInput";
@@ -6,7 +7,27 @@ import FoodItems from "./components/FoodItems";
 function App() {
   let foodItems = ["Dal Makhani", "Roti", "Green Vegetables", "Milk", "Salad"];
 
-  let textToShow = "Food entered by user.";
+  //let  = "Food entered by user.";
+
+  /**
+   * Here we learned the new concept useState(). uesState is one of the react hook.
+   * So useState() is used to maintain the latest change value or we can say the current value.
+   * Here textStateArray recevied two elements from useState() method.
+   * First one is current or we can say latest value and another is state method.
+   * So when first time App component is load or render useStae have no values and due to this
+   * in text show it will save undefined value.
+   * After that once user something type in input box of FoodInput component it will call onChange event.
+   * When this event is called new value or we can say new input received from user.
+   * At that time stateTextStae method of useState is called that is we written in onChange event
+   * and it will received the new input value.
+   * And that automatically assigned to textToShow variable.
+   * Here in useState("Default value") we can pass initiall default value also.
+   */
+  let textStateArray = useState();
+  let textToShow = textStateArray[0];
+  let setTextState = textStateArray[1];
+
+  console.log(`Current value of textSate is :: ${textToShow}`);
   /**
    * Here we seen the use of passing function as props to child.
    * Here we passed handleonChangeEvent to chile FoodInput.
@@ -15,7 +36,7 @@ function App() {
    */
   const onChangeEventCalled = (event) => {
     console.log(event.target.value);
-    textToShow = event.target.value;
+    setTextState(event.target.value);
     /**
      * Above we assigned dynamic value to variable textToShow.
      * That will change in variable but not in DOM because, rendring will done only ones.
