9cc4b33e96bf6c98eb6579f54c355fd3441caeff
JatinbVyas <vyassjatin@gmail.com>
2025-03-05 21:00:23 +0530

    seen the use of useReducer instead of useState for additem

--- 15_Topic 36 Use Reducer/src/App.jsx
+++ 15_Topic 36 Use Reducer/src/App.jsx
@@ -5,18 +5,50 @@ import TodoItemContainer from "./components/TodoItemContainer";
 import { useState } from "react";
 import WelcomeMsg from "./components/WelocomMsg";
 import TodoItemsContext from "./store/todo-items-store";
+import { useReducer } from "react";
+
+/**
+ *
+ * @param {this is the currentItem/currentValue of useState} currtodoItem
+ * @param {this is the action object received when dispatch the item} action
+ * @returns it returns the updated new value and that is directly set to your main variable that is define as first value in useReducer here it is todolist.
+ * here in action method we received two object first one is current/existing value and action object.
+ * so based on type we can define the login as we did earlier in useState.
+ */
+const todoItemReducer = (currtodoItem, action) => {
+  let newtodoItem = currtodoItem;
+  if (action.type === "NEW_ITEM") {
+    newtodoItem = [
+      ...currtodoItem,
+      { name: action.payload.itemName, dueDate: action.payload.dueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+  }
+  return newtodoItem;
+};
 
 function App() {
   const InitialTodolist = [];
 
-  const [todolist, settodolist] = useState(InitialTodolist);
+  //const [todolist, settodolist] = useState(InitialTodolist);
+  /**
+   * Here we learned new concept useReducer() hook.
+   * This hook can be used instead of useState();
+   * This useReducer take two items first is fucntion and second is your initial value.useReducer(todoItemReducer, []);
+   * And it returns two value same as useState(), first is yout value and second is displatchObject/dispatchmethod instated of set method
+   * for this we need to define one fucntion that handles the action and that fuction pass as first item in useReducer(todoItemReducer)
+   * so when addNewItem function is called here we define one object in which we pass first type, it means what kind of action is this and payload as second in which our new item/value get passed.
+   * and then dispatch method get called and pass this new object and this will called the action method todoItemReducer.
+   */
+  const [todolist, dispatchtodoList] = useReducer(todoItemReducer, []);
 
   const addNewItem = (itemName, dueDate) => {
-    console.log("New items is::", itemName, dueDate);
-    settodolist((currValue) => {
-      const newtodoItem = [...currValue, { name: itemName, dueDate: dueDate }];
-      return newtodoItem;
-    });
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: { itemName, dueDate },
+    };
+
+    dispatchtodoList(newItemAction);
   };
 
   const deleteItem = (deleteItemName) => {
@@ -28,24 +60,6 @@ function App() {
   };
 
   return (
-    /**
-     * Here we learning new concept of Context Provider or we can say Context API
-     * Context API is one kind of storage from any component can access data that is stored in it.
-     * For we created one file in src/store/todo-items-store.jsx and export it.
-     * we can define as below <TodoItemsContext.Provider value={[]}></TodoItemsContext.Provider> so all thew component
-     * that comes under this can access that context provider value. we need pass value prop to context provider itself also.
-     *
-     * Now we seen that state variable can directly pass to context provider and this way we can use context provide API
-     * with state variable. Whenever state variable updates it pass new value to all components with the use of context provider value attribute.
-     *
-     * Again we learn new concept in context API is that , we can pass fuctions props also as context provider value.
-     * Context provider can send any thing, it could be value, array, method anything.
-     * So here we passed in one JS objecxt first is array of todo items, second is function of addnewitem and third is function of
-     * deleteitem.
-     * so now both fucntions no need to pass as props in component addtodo and todocontainer.
-     *
-     * So at the end all three value itemlist , add function and del function we were passing thourgh props instead of this now we store it in context provider and pass it thourgh value attr.
-     */
     <TodoItemsContext.Provider
       value={{ itemData: todolist, addItem: addNewItem, delItem: deleteItem }}
     >
