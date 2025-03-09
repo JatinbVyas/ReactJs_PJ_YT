2f910f8f39fcb65799fd3fc0c32804635e70e922
JatinbVyas <vyassjatin@gmail.com>
2025-03-09 21:14:45 +0530

    learn the use of useEffect

--- 17_Topic 39 Use Effect hook/src/components/PostList.jsx
+++ 17_Topic 39 Use Effect hook/src/components/PostList.jsx
@@ -1,21 +1,25 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect } from "react";
 import PostViewCP from "./PostViewCP";
 import { Postlist as postData } from "../store/Postliststore";
 import WelcomeMSG from "./WelcomeMSG";
 
 const PostList = () => {
   const { postList, addDefaultPost } = useContext(postData);
-  const [dataFetched, setDataFetched] = useState(false);
-
-  if (!dataFetched) {
+  /**
+   * here we seet the new hook that is useEffect hook.
+   * This hook take two aregument , first one is function in which you can write anything.
+   * and second is dependancy list.
+   * If you pass empty array in second argument means no dependancy then this will render only once at initial stage.
+   * if you pass any props or state then whenever state or props change it will render the function that is written in first argument.
+   */
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addDefaultPost(data.posts);
       });
+  }, []);
 
-    setDataFetched(true);
-  }
   return (
     <>
       {postList.length == 0 && <WelcomeMSG></WelcomeMSG>}
