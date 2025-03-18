6ebeb73506ee13d274e249e644fd7d1654a970f8
JatinbVyas <vyassjatin@gmail.com>
2025-03-18 22:55:07 +0530

    removed sidebar onclick listner and implemented href link based no paths created in main.jsx

--- 22_Topic 46 React Router/src/components/SidebarCP.jsx
+++ 22_Topic 46 React Router/src/components/SidebarCP.jsx
@@ -17,27 +17,16 @@ const SidebarCP = ({ selectedTab, setSelectedTab }) => {
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item" onClick={() => setSelectedTab("Home")}>
-            <a
-              href="#"
-              className={`nav-link text-white ${
-                selectedTab === "Home" && "active"
-              }`}
-              aria-current="page"
-            >
+          <li className="nav-item">
+            <a href="/" className="nav-link text-white" aria-current="page">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#home"></use>
               </svg>
               Home
             </a>
           </li>
-          <li onClick={() => setSelectedTab("createpost")}>
-            <a
-              href="#"
-              className={`nav-link text-white ${
-                selectedTab === "createpost" && "active"
-              }`}
-            >
+          <li>
+            <a href="/create-post" className="nav-link text-white">
               <svg className="bi pe-none me-2" width="16" height="16">
                 <use xlinkHref="#speedometer2"></use>
               </svg>
