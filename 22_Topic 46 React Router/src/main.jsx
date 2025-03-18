b771536db57032e2638d58ebc8e2057727dcbefc
JatinbVyas <vyassjatin@gmail.com>
2025-03-18 20:58:53 +0530

    learned to give first defaul path using RouterProvider and createBrowserRouter

--- 22_Topic 46 React Router/src/main.jsx
+++ 22_Topic 46 React Router/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 
+/**
+ * Here we learned new concept of ReactRouter. React Router used define paths for your pages with single page application.
+ * For achive this first we need to import RouterProvider and createBrowserRouter from ract-router-dom package that is installed in your project using npm install react-router-dom.
+ * After this we need to create route path object using createBrowserRouter in which you need to define various path of your application in array.
+ * And then instaed of direct define App component in strictmode define RouterProvider component and in which give router property the path array that you created from vreateBrowserRouter.
+ */
+const routePaths = createBrowserRouter([{ path: "/", element: <App></App> }]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routePaths}></RouterProvider>
   </StrictMode>
 );
