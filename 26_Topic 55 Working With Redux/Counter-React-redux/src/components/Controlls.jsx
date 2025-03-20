b3e3876daabba336c61304d507880000bfdae647
JatinbVyas <vyassjatin@gmail.com>
2025-03-20 19:06:07 +0530

    completed the +1 and -1 functionality using redux

--- 26_Topic 55 Working With Redux/Counter-React-redux/src/components/Controlls.jsx
+++ 26_Topic 55 Working With Redux/Counter-React-redux/src/components/Controlls.jsx
@@ -1,10 +1,26 @@
+import { useDispatch } from "react-redux";
+
 const Controlls = () => {
+  //useDispatch hook is used to displatch that action, this returns one dispatch method.
+  const dispatch = useDispatch();
+  const onIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const onDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
   return (
     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+      <button
+        type="button"
+        className="btn btn-primary btn-lg px-4 gap-3"
+        onClick={onIncrement}
+      >
         Increment by +1
       </button>
-      <button type="button" className="btn btn-success">
+      <button type="button" className="btn btn-success" onClick={onDecrement}>
         Decrement by -1
       </button>
     </div>
--- 26_Topic 55 Working With Redux/Counter-React-redux/src/store/index.js
+++ 26_Topic 55 Working With Redux/Counter-React-redux/src/store/index.js
@@ -1,10 +1,21 @@
 import { createStore } from "redux";
 
 const INITIAL_VALUE = { coutner: 0 };
-
+/**
+ *
+ * This is a redux reducer that handles all actions and return the current store value.
+ * this received store current value and action.
+ */
 const counterReducer = (store = INITIAL_VALUE, action) => {
+  if (action.type === "INCREMENT") {
+    return { coutner: store.coutner + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { coutner: store.coutner - 1 };
+  }
   return store;
 };
+
+// here we created store
 const counterStore = createStore(counterReducer);
 
 export default counterStore;
