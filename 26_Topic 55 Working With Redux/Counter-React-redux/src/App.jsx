f05b48af9d80a2c7192a7128059561be2204d7f9
JatinbVyas <vyassjatin@gmail.com>
2025-03-20 18:39:39 +0530

    component work is completed

--- 26_Topic 55 Working With Redux/Counter-React-redux/src/App.jsx
+++ 26_Topic 55 Working With Redux/Counter-React-redux/src/App.jsx
@@ -1,26 +1,20 @@
 import "./App.css";
+import ContainerCP from "./components/ContainerCP";
+import Controlls from "./components/Controlls";
+import DisplayCounter from "./components/DisplayCounter";
+import HeaderCP from "./components/HeaderCP";
 
 function App() {
   return (
     <>
-      <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-              Primary button
-            </button>
-            <button type="button" class="btn btn-outline-secondary btn-lg px-4">
-              Secondary
-            </button>
+      <div className="px-4 py-5 my-5 text-center">
+        <ContainerCP>
+          <HeaderCP></HeaderCP>
+          <div className="col-lg-6 mx-auto">
+            <DisplayCounter></DisplayCounter>
+            <Controlls></Controlls>
           </div>
-        </div>
+        </ContainerCP>
       </div>
     </>
   );
---
+++ 26_Topic 55 Working With Redux/Counter-React-redux/src/components/Controlls.jsx
@@ -0,0 +1,14 @@
+const Controlls = () => {
+  return (
+    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+      <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
+        Increment by +1
+      </button>
+      <button type="button" class="btn btn-success">
+        Decrement by -1
+      </button>
+    </div>
+  );
+};
+
+export default Controlls;
