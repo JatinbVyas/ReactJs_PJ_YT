923dfda6f8f20467021787384d7cb70490ab5107
JatinbVyas <vyassjatin@gmail.com>
2025-03-22 21:19:54 +0530

    center display item component designed and one hard code item added

--- Projects_Assignments/8.0 Myntra Project/Myntra-Frontend/src/App.jsx
+++ Projects_Assignments/8.0 Myntra Project/Myntra-Frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import FooterCP from "./components/FooterCP";
 import HeaderCP from "./components/HeaderCP";
+import HomeItemCP from "./components/HomeItemCP";
 
 function App() {
+  const item = {
+    id: "001",
+    image: "images/1.jpg",
+    company: "Carlton London",
+    item_name: "Rhodium-Plated CZ Floral Studs",
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    return_period: 14,
+    delivery_date: "10 Oct 2023",
+    rating: {
+      stars: 4.5,
+      count: 1400,
+    },
+  };
   return (
     <>
       <HeaderCP></HeaderCP>
       <main>
-        <div className="items-container"></div>
+        <div className="items-container">
+          <HomeItemCP item={item}></HomeItemCP>
+        </div>
       </main>
       <FooterCP></FooterCP>
     </>
