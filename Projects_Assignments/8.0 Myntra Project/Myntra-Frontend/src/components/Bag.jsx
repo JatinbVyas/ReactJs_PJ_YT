ca82ee95ccb624fd035592c7a87a4687d27e7c96
JatinbVyas <vyassjatin@gmail.com>
2025-03-22 21:10:49 +0530

    initial components created

--- Projects_Assignments/8.0 Myntra Project/Myntra-Frontend/src/components/Bag.jsx
+++ Projects_Assignments/8.0 Myntra Project/Myntra-Frontend/src/components/Bag.jsx
@@ -1,12 +1,17 @@
+import FooterCP from "./FooterCP";
+import HeaderCP from "./HeaderCP";
+
 const BagCP = () => {
   return (
     <>
+      <HeaderCP></HeaderCP>
       <main>
         <div className="bag-page">
           <div className="bag-items-container"></div>
           <div className="bag-summary"></div>
         </div>
       </main>
+      <FooterCP></FooterCP>
     </>
   );
 };
